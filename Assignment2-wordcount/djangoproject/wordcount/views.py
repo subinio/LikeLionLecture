5611fe5002bbf8fd07a8cf06965e4f0c4f4883e4
subinio <subinjin22@gmail.com>
2019-06-03 05:28:38 +0900

    add function and styling

--- Assignment2-wordcount/djangoproject/wordcount/views.py
+++ Assignment2-wordcount/djangoproject/wordcount/views.py
@@ -12,6 +12,7 @@ def result(request):
     text = request.GET['fulltext']
     words = text.split()
     word_dictionary = {}
+    sentence_cnt = 0
 
     for word in words:
         if word in word_dictionary:
@@ -19,4 +20,8 @@ def result(request):
         else:
             word_dictionary[word]=1
 
-    return render(request,'result.html', {'full':text, 'total':len(words), 'dictionary':word_dictionary.items()})
+    for dot in text:
+        if dot == '.':
+            sentence_cnt+=1
+
+    return render(request,'result.html', {'full':text, 'total':len(words), 'dictionary':word_dictionary.items(), 'letter':len(text), 'sentence':sentence_cnt})
